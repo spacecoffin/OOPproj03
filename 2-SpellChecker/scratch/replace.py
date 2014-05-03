1358f33d2420c1c8dec374cd4bbd01f6ca4e1f89
spacecoffin <reed.rosenberg@gmail.com>
2014-05-03 04:34:12 -0400

    almost there
    cool

--- 2-SpellChecker/scratch/replace.py
+++ 2-SpellChecker/scratch/replace.py
@@ -1,9 +1,28 @@
 suggestion_list = []
 
-for i in range(len(word)):
-    while len(suggestion_list) < 10:
-        
-
+if len(suggestion_list) < 10:
+    # Swapped consecutive letter check
+    for i in range(len(word)-1):
+        if self.adict.dict_verify(word[:i] + word[i+1] + word[i] + word[i+2:]):
+            suggestion_list.append(
+                word[:i] + word[i+1] + word[i] + word[i+2:])
+if len(suggestion_list) < 10:
+    # Extra letter check
+    for i in range(len(word)):
+        if self.adict.dict_verify(word[:i] + word[i+1:]):
+            suggestion_list.append(word[:i] + word[i+1:])
+if len(suggestion_list) < 10:
+    # Substituted letter check
+    for i in range(len(word)):
+        for l in range(26):
+            if self.adict.dict_verify(word[:i] + chr(l+97) + word[i+1:]):
+                suggestion_list.append(word[:i] + chr(l+97) + word[i+1:])
+if len(suggestion_list) < 10:
+    # Missing letter check
+    for i in range(len(word)+1):
+        for l in range(26):
+            if self.adict.dict_verify(word[:i] + chr(l+97) + word[i:]):
+                suggestion_list.append(word[:i] + chr(l+97) + word[i:])
 
 # EXTRA
 # word[:i] + word[i+1:]
@@ -26,3 +45,21 @@ for i in range(len(word)):
 #for i in range(len(word)):
 #    for l in range(26):
 #        print(word[:i] + chr(l+97) + word[i+1:])
+
+"""
+if True:
+    # Swapped consecutive letter check
+    for i in range(len(word)-1):
+        print('Swap: {}'.format(word[:i] + word[i+1] + word[i] + word[i+2:]))
+    # Extra letter check
+    for i in range(len(word)):
+        print('Extra: {}'.format(word[:i] + word[i+1:]))
+    # Substituted letter check
+    for i in range(len(word)):
+        for l in range(26):
+            print('Subbed: {}'.format(word[:i] + chr(l+97) + word[i+1:]))
+    # Missing letter check
+    for i in range(len(word)+1):
+        for l in range(26):
+            print('Missing: {}'.format(word[:i] + chr(l+97) + word[i:]))
+"""
--- 2-SpellChecker/spellCheck.py
+++ 2-SpellChecker/spellCheck.py
@@ -40,14 +40,49 @@ class SpellChecker:
             list_total = self.stats_dict[list_length]
             print('{}{:<3}{}{:>5}'.format(
                 ' '*3, list_length, ' '*12, list_total))
+        print()
     
-    def askuser(word):
+    def ver_mis(self, word):
+    # Missing letter function
+        for i in range(len(word)+1):
+            for l in range(26):
+                temp_word = word[:i] + chr(l+97) + word[i:]
+                if self.adict.dict_verify(temp_word):
+                    yield temp_word
+        else: return None
+    
+    def ver_sub(self, word):
+    # Substituted letter function
+        for i in range(len(word)):
+            for l in range(26):
+                temp_word = word[:i] + chr(l+97) + word[i+1:]
+                if self.adict.dict_verify(temp_word):
+                    yield temp_word
+        else: return None
+    
+    def ver_ext(self, word):
+    # Extra letter function
+        for i in range(len(word)):
+            temp_word = word[:i] + word[i+1:]
+            if self.adict.dict_verify(temp_word):
+                yield temp_word
+        else: return None
+    
+    def ver_swp(self, word):
+    # Swapped consecutive letter function
+        for i in range(len(word)-1):
+            temp_word = word[:i] + word[i+1] + word[i] + word[i+2:]
+            if self.adict.dict_verify(temp_word):
+                yield temp_word
+        else: return None
+    
+    def askuser(self, word):
         """
         if the word is not found in the dictionary, user input is
         requested
         """
         
-        print (('\n\t\t\t%s\n') % word)
+        print (('\n\t\t\t"%s" is unknown.\n') % word)
     
         query = 'replace(R), replace all(P), ignore(I), ignore all(N), exit(E): '
     
@@ -57,18 +92,129 @@ class SpellChecker:
     
         newword = word
         if choice in  ['R','r','P','p']:
-            suggestion_list = []
-            for index in word:
-                while len(suggestion_list) < 10:
-                    if self.adict.primary.lookup(word[index+1:]):
-                    
+            self.suggestion_list = []
                 
+            # Missing letter check
+            for i in range(len(word)+1):
+                if len(self.suggestion_list) < 10:
+                    for l in range(26):
+                        if len(self.suggestion_list) < 10:
+                            temp_word = word[:i] + chr(l+97) + word[i:]
+                            if self.adict.dict_verify(temp_word)\
+                            and not temp_word in self.suggestion_list:
+                                self.suggestion_list.append(temp_word)
+                        else:
+                            break
+                else:
+                    break
             
-            #newword = input ('Replacement word: ')
+            # Substituted letter check
+            for i in range(len(word)):
+                if len(self.suggestion_list) < 10:
+                    for l in range(26):
+                        if len(self.suggestion_list) < 10:
+                            temp_word = word[:i] + chr(l+97) + word[i+1:]
+                            if self.adict.dict_verify(temp_word)\
+                            and not temp_word in self.suggestion_list:
+                                self.suggestion_list.append(temp_word)
+                        else:
+                            break
+                else:
+                    break
+                
+            # Extra letter check
+            for i in range(len(word)):
+                if len(self.suggestion_list) < 10:
+                    temp_word = word[:i] + word[i+1:]
+                    if self.adict.dict_verify(temp_word) \
+                    and not temp_word in self.suggestion_list:
+                        self.suggestion_list.append(temp_word)
+                else:
+                    break
+                
+            # Swapped consecutive letter check
+            for i in range(len(word)-1):
+                if len(self.suggestion_list) < 10:
+                    temp_word = word[:i] + word[i+1] + word[i] + word[i+2:]
+                    if self.adict.dict_verify(temp_word) \
+                    and not temp_word in self.suggestion_list:
+                        self.suggestion_list.append(temp_word)
+                else:
+                    break
             
-            # REPLACEMENT BIT UP HERE
+            while len(self.suggestion_list) < 10:
+                for dist1 in self.ver_mis(word):
+                    for dist2 in self.ver_mis(dist1):
+                        if not dist2 in self.suggestion_list:
+                            self.suggestion_list.append(dist2)
+                    for dist2 in self.ver_sub(dist1):
+                        if not dist2 in self.suggestion_list:
+                            self.suggestion_list.append(dist2)
+                    for dist2 in self.ver_ext(dist1):
+                        if not dist2 in self.suggestion_list:
+                            self.suggestion_list.append(dist2)
+                    for dist2 in self.ver_swp(dist1):
+                        if not dist2 in self.suggestion_list:
+                            self.suggestion_list.append(dist2)
+                for dist1 in self.ver_sub(word):
+                    for dist2 in self.ver_mis(dist1):
+                        if not dist2 in self.suggestion_list:
+                            self.suggestion_list.append(dist2)
+                    for dist2 in self.ver_sub(dist1):
+                        if not dist2 in self.suggestion_list:
+                            self.suggestion_list.append(dist2)
+                    for dist2 in self.ver_ext(dist1):
+                        if not dist2 in self.suggestion_list:
+                            self.suggestion_list.append(dist2)
+                    for dist2 in self.ver_swp(dist1):
+                        if not dist2 in self.suggestion_list:
+                            self.suggestion_list.append(dist2)
+                for dist1 in self.ver_ext(word):
+                    for dist2 in self.ver_mis(dist1):
+                        if not dist2 in self.suggestion_list:
+                            self.suggestion_list.append(dist2)
+                    for dist2 in self.ver_sub(dist1):
+                        if not dist2 in self.suggestion_list:
+                            self.suggestion_list.append(dist2)
+                    for dist2 in self.ver_ext(dist1):
+                        if not dist2 in self.suggestion_list:
+                            self.suggestion_list.append(dist2)
+                    for dist2 in self.ver_swp(dist1):
+                        if not dist2 in self.suggestion_list:
+                            self.suggestion_list.append(dist2)
+                for dist1 in self.ver_swp(word):
+                    for dist2 in self.ver_mis(dist1):
+                        if not dist2 in self.suggestion_list:
+                            self.suggestion_list.append(dist2)
+                    for dist2 in self.ver_sub(dist1):
+                        if not dist2 in self.suggestion_list:
+                            self.suggestion_list.append(dist2)
+                    for dist2 in self.ver_ext(dist1):
+                        if not dist2 in self.suggestion_list:
+                            self.suggestion_list.append(dist2)
+                    for dist2 in self.ver_swp(dist1):
+                        if not dist2 in self.suggestion_list:
+                            self.suggestion_list.append(dist2)
+                break
             
-    
+            self.suggestion_list = self.suggestion_list[:10]
+            
+            for item in range(len(self.suggestion_list)):
+                print('( {} ) {}'.format(item, self.suggestion_list[item]))
+            print('( {} ) Use my replacement'.format(len(self.suggestion_list)))
+            
+            while True:
+                replace_choice = int(input('Your choice:  '))
+                if replace_choice == len(self.suggestion_list):
+                    newword = input('Replacement word: ')
+                    break
+                elif replace_choice < len(self.suggestion_list):
+                    newword = self.suggestion_list[replace_choice]
+                    break
+                else:
+                    print('Choose option in range 0-{}'.format(
+                        len(self.suggestion_list)))
+            
         return (choice, newword)
     
     def spellcheck(self, text="mytext"):
@@ -104,7 +250,7 @@ class SpellChecker:
                             word = word[:-1] # strip trailing apostrophes
                         newword = self.adict.verify(word)
                         if newword is None:
-                            choice, newword = askuser(word)
+                            choice, newword = self.askuser(word)
                             outfile.write(newword)
                             processinput(choice, newword, word)
                         else:
@@ -125,7 +271,7 @@ class SpellChecker:
                         word = word[:-1] # strip trailing apostrophes
                     newword = self.adict.verify(word)
                     if newword is None:
-                        choice, newword = askuser(word)
+                        choice, newword = self.askuser(word)
                         outfile.write(newword); outfile.write(char)
                         processinput(choice, newword, word)
                     else:
